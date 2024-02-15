0321f16a29524c1b2f19eb8c13007d9257ed1b91
Alex Forsythe <awforsythe@gmail.com>
2024-02-15 11:58:32 -0500

    Document internal packages

---
+++ internal/admin/doc.go
@@ -0,0 +1,3 @@
+// Package admin implements an admin-only HTTP server that allows the broadcaster to
+// directly modify the state of the service, including registering/updating clips, etc.
+package admin
---
+++ internal/state/doc.go
@@ -0,0 +1,4 @@
+// Package state implements an HTTP server that allows any user (no auth required) to
+// access the current state of the remix service, including the listing of available
+// clips, current queue/playback state, etc.
+package state
